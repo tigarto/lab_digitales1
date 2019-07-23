2b800c3894b39de54cdec6b283d4abc4531ebe5a
Unknown <helpu1409@gmail.com>
2019-07-22 20:02:22 -0500

    agregando nuevos archivos

---
+++ 2/full_adder/fulladder_dataflow.vhd
@@ -0,0 +1,30 @@
+--------------------------------------------
+-- Module Name: fulladder_dataflow
+--------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+library UNISIM;
+use UNISIM.VComponents.all;
+
+Entity fulladder_dataflow Is
+port(
+		a  	 : in STD_LOGIC;
+		b  	 : in STD_LOGIC;
+		cin  : in STD_LOGIC;
+		s 	 : out STD_LOGIC;
+		cout : out STD_LOGIC
+	);
+end fulladder_dataflow;
+
+Architecture behavior of fulladder_dataflow Is
+
+begin
+    process (a, b, cin) 
+    begin
+        s <= a xor b xor cin;
+        cout <= (a and b) or (a and cin) or (b and cin);
+	end process;
+	
+end behavior;
