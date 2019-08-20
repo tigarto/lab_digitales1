8171773504e545eac59dff207548962f6ad9e700
Unknown <helpu1409@gmail.com>
2019-08-19 19:01:54 -0500

    agregando la parte de verificacion
    Aun se encuentra incompleto y con errores

--- 3/ejemplos_clase/ROM/rom_tb.vhd
+++ 3/ejemplos_clase/ROM/rom_tb.vhd
@@ -6,6 +6,8 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use STD.textio.all; --for line
+use IEEE.std_logic_textio.all; --for write
 
 -- library UNISIM;
 -- use UNISIM.VComponents.all;
@@ -33,8 +35,30 @@ Architecture behavior of rom_tb Is
 		);
 	end Component;
 
+	
+	procedure expected_data (
+		proc_addr : in integer range 0 to WORDS-1;
+		proc_data : out std_logic_vector(DATA_SIZE -1 downto 0)) is
+
+	type vector_array is array (0 to WORDS - 1) of std_logic_vector (DATA_SIZE-1 downto 0);
+
+	CONSTANT memory: vector_array := ( 
+			"00000000",
+			"00000010",
+			"00000100",
+			"00001000", 
+			"00010000",
+			"00100000",
+			"01000000",
+			"10000000");
+	begin	
+		proc_data := memory(proc_addr);
+	end expected_data;
+
 
 begin
+
+	
 
 	DUT: rom 
 	generic map ( 
@@ -56,5 +80,14 @@ begin
 		end loop;
 		wait; -- Veer despues
 	end process;
+
+	Verific: process(addr_s)
+	variable proc_data : std_logic_vector(DATA_SIZE -1 downto 0);
+	variable s : line;
+	begin
+		
+	end process; 
+
+	  
 
 end behavior;
